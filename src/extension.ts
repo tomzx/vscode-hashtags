818242feebe154d466ec60c674cefd2e7d54938f
Tom Rochette <roctom@gmail.com>
2018-05-04 14:00:35 -0400

    Trigger autocompletion on hash tag.

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,21 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import { HashTagsCompletionItemProvider } from './hashtags_completion_item_provider';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "hashtags" is now active!');
+	console.log('Congratulations, your extension "hashtags" is now active!');
 
-    let findCommand = vscode.commands.registerCommand('hashtags.find', () => {
-        vscode.window.showInformationMessage('Find!');
-    });
+	const selector = '*';
+	const provider = new HashTagsCompletionItemProvider();
+	const hashtagsTrigger = vscode.languages.registerCompletionItemProvider(selector, provider, '#');
 
-    context.subscriptions.push(findCommand);
+	const findCommand = vscode.commands.registerCommand('hashtags.find', () => {
+		vscode.window.showInformationMessage('Find!');
+	});
+
+	context.subscriptions.push(hashtagsTrigger);
+	context.subscriptions.push(findCommand);
 }
 
 export function deactivate() {
---
+++ src/hashtags_completion_item_provider.ts
@@ -0,0 +1,20 @@
+import { IConfiguration } from "./configuration";
+import { CompletionItemProvider, TextDocument, Position, CancellationToken, CompletionItem, CompletionContext, ProviderResult, CompletionList, workspace, CompletionItemKind } from "vscode";
+
+export class HashTagsCompletionItemProvider implements CompletionItemProvider {
+	public provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken, context: CompletionContext): ProviderResult<CompletionItem[] | CompletionList> {
+		const configuration = workspace.getConfiguration().get<IConfiguration>('hashtags');
+
+		if (!configuration) {
+			return;
+		}
+
+		const items: CompletionItem[] = [];
+
+		for (let tag of configuration.tags) {
+			items.push(new CompletionItem(tag, CompletionItemKind.Keyword));
+		}
+
+		return items;
+	}
+}
