1c2614aba4982fa2b815ef00e944fd79afbeaffb
Tom Rochette <roctom@gmail.com>
2018-06-09 14:37:23 -0400

    Update logo url to use https.

--- src/extension.ts
+++ src/extension.ts
@@ -2,19 +2,24 @@
 
 import * as vscode from 'vscode';
 import { HashTagsCompletionItemProvider } from './hashtags_completion_item_provider';
+import { HashtagsDocumentLinkProvider } from './hashtags_document_link_provider';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "hashtags" is now active!');
 
 	const selector = '*';
-	const provider = new HashTagsCompletionItemProvider();
-	const hashtagsTrigger = vscode.languages.registerCompletionItemProvider(selector, provider, '#');
+	const completionItemProvider = new HashTagsCompletionItemProvider();
+	const completionItemProviderDisposable = vscode.languages.registerCompletionItemProvider(selector, completionItemProvider, '#');
+
+	const documentLinkProvider = new HashtagsDocumentLinkProvider();
+	const documentLinkProviderDisposable = vscode.languages.registerDocumentLinkProvider(selector, documentLinkProvider);
 
 	const findCommand = vscode.commands.registerCommand('hashtags.find', () => {
 		vscode.window.showInformationMessage('Find!');
 	});
 
-	context.subscriptions.push(hashtagsTrigger);
+	context.subscriptions.push(completionItemProviderDisposable);
+	context.subscriptions.push(documentLinkProviderDisposable);
 	context.subscriptions.push(findCommand);
 }
 
